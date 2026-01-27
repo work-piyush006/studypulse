dcc7c40d3ccadb3100dd8f376a7a1988f762719a
work-piyush006 <work.piyush006@gmail.com>
2026-01-28 00:13:25 +0530

    Enable core library desugaring
    Enable core library desugaring in build configuration.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,9 +11,11 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+    isCoreLibraryDesugaringEnabled = true
+}
+    
 
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
