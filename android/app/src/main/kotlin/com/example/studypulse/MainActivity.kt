938afa5240ccb0fe427b98054bb0fd341326ffa6
work-piyush006 <work.piyush006@gmail.com>
2026-01-29 11:33:22 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/studypulse/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/studypulse/MainActivity.kt
@@ -1,35 +1,5 @@
 package com.example.studypulse
 
-import android.content.Intent
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity() {
-
-    private val CHANNEL = "studypulse/notifications"
-
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-
-        MethodChannel(
-            flutterEngine.dartExecutor.binaryMessenger,
-            CHANNEL
-        ).setMethodCallHandler { call, result ->
-            when (call.method) {
-                "openInbox" -> {
-                    openInbox()
-                    result.success(true)
-                }
-                else -> result.notImplemented()
-            }
-        }
-    }
-
-    private fun openInbox() {
-        val intent = Intent(this, MainActivity::class.java)
-        intent.putExtra("route", "inbox")
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
-        startActivity(intent)
-    }
-}
+class MainActivity : FlutterActivity()
