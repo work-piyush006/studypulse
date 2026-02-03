79e6d084699d74f725ddcc156e7807a0ec49303c
work-piyush006 <work.piyush006@gmail.com>
2026-02-03 20:23:36 +0530

    Update namespace and application ID for Android app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.studypulse"
+    namespace = "com.studypulse.myapp"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -25,7 +25,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.studypulse"
+        applicationId = "com.studypulse.myapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
