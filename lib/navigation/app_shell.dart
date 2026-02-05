189d5477716066742846982dd35d6eecb5faa620
work-piyush006 <work.piyush006@gmail.com>
2026-02-05 15:48:44 +0530

    Implement AppShell with bottom navigation and pages

---
+++ lib/navigation/app_shell.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../home.dart';
+import '../screens/about.dart';
+import '../screens/settings.dart';
+
+class AppShell extends StatefulWidget {
+  const AppShell({super.key});
+
+  @override
+  State<AppShell> createState() => _AppShellState();
+}
+
+class _AppShellState extends State<AppShell> {
+  int _index = 0;
+
+  final _pages = const [
+    Home(),
+    AboutScreen(),
+    SettingsScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _index,
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _index,
+        onTap: (i) => setState(() => _index = i),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.info), label: 'About'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
+        ],
+      ),
+    );
+  }
+}
